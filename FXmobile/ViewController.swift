85efcf1c02a383d05e6dfa7f2a49dcd9789c0f35
Ra√∫l √Ålvarez Gonz√°lez <raulalgo@gmail.com>
2015-09-30 16:41:11 +0100

    Initial Commit

---
+++ FXmobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FXmobile
+//
+//  Created by Raul Alvarez on 30/09/2015.
+//  Copyright © 2015 Raul Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
